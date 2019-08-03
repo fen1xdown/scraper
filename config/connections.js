ebb6c8d2532b181e88618b940fbb7e7730413dd8
fen1xdown <cameronleecalder@gmail.com>
2019-08-03 16:24:55 -0500

    201908031624 commit

--- config/connections.js
+++ config/connections.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://heroku_l6drmvct:58e0a6rgkm90rdse98d9rj5fc6@ds145208.mlab.com:45208/heroku_l6drmvct", function(err) {
+mongoose.connect("mongodb://git.heroku.com/fen1x-scraper.git", function(err) {
 	if(err) throw err;
 	console.log('database connected');
 });
