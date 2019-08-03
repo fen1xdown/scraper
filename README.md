dae1b68b372f9910ef5323c5916f905aa0352c08
fen1xdown <cameronleecalder@gmail.com>
2019-08-03 01:07:59 -0500

    201908030107 commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # scraper
-
-This site is intended to find, scrape, and display information about a website (website name goes here) and display it in the console.
--- controllers/news.js
+++ controllers/news.js
@@ -1,4 +1,3 @@
-// NPM Modules
 
 var express = require('express');
 var path = require('path');
@@ -8,21 +7,15 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Promise = require("bluebird");
 
-// Assign Mongoose promise
 mongoose.Promise = Promise;
 
-// Mongodb models
 var Articles = require("../models/articles");
 var Comments = require("../models/comments");
 
-// Website To Be Scraped
-var url = "http://www.goodnewsnetwork.org/latest-news/";
+var url = "http://www.cnn.com";
 
-// Test Route To Verify Scraping Works From Route
 router.get('/test', function(req, res) {
-    // body of the html with request
     request(url, function(error, response, html) {
-        // load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(html);
 		var result = [];
 		$(".span6").each(function(i, element) {
@@ -43,17 +36,14 @@ router.get('/test', function(req, res) {
     });
 });
 
-// Default route renders the index handlebars view
 router.get('/', function(req, res){
 	res.render('index');
 });
 
-// Scrape the website and assign stories to the database. Checks to verify story has not been added previously.
 router.get('/scrape', function(req, res){
     request(url, function(error, response, html) {	
         var $ = cheerio.load(html);
 		var result = [];
-		// Scrape website
 		$(".span6").each(function(i, element) {
 		    var title = $(element).find("a").find("img").attr("title");
 		    var imgLink = $(element).find("a").find("img").attr("src");
@@ -66,7 +56,6 @@ router.get('/scrape', function(req, res){
 				storyLink: storyLink,
 				summary: summary
 			});	
-			// Check database to see if story saved previously to database
 			Articles.findOne({'title': title}, function(err, articleRecord) {
 				if(err) {
 					console.log(err);
@@ -85,7 +74,6 @@ router.get('/scrape', function(req, res){
     });	
 });
 
-// Get all current articles in database
 router.get('/articles', function(req, res){
 	Articles.find().sort({ createdAt: -1 }).exec(function(err, data) { 
 		if(err) throw err;
@@ -93,7 +81,6 @@ router.get('/articles', function(req, res){
 	});
 });
 
-// Get all comments for one article
 router.get('/comments/:id', function(req, res){
 	Comments.find({'articleId': req.params.id}).exec(function(err, data) {
 		if(err) {
@@ -104,7 +91,6 @@ router.get('/comments/:id', function(req, res){
 	});
 });
 
-// Add comment for article
 router.post('/addcomment/:id', function(req, res){
 	console.log(req.params.id+' '+req.body.comment);
 	Comments.create({
@@ -120,7 +106,6 @@ router.post('/addcomment/:id', function(req, res){
 	});
 });
 
-// Delete comment for article
 router.get('/deletecomment/:id', function(req, res){
 	console.log(req.params.id)
 	Comments.remove({'_id': req.params.id}).exec(function(err, data){
